f89e7fb0d1b73187d91433425f467d48db67f5b6
ainaarawaida <admin@ainaarawaida.com>
2022-07-25 11:25:45 +0800

    first commit

---
+++ includes/class-svelteboilerplate-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       svelteBoilerplate
+ * @since      1.0.0
+ *
+ * @package    Svelteboilerplate
+ * @subpackage Svelteboilerplate/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Svelteboilerplate
+ * @subpackage Svelteboilerplate/includes
+ * @author     Luqman Nordin <me@luqmannordin.com>
+ */
+class Svelteboilerplate_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
